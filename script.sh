895b125b04b7b3eb4b631a313f512336885b44d9
Martin Balko <m.balko8@gmail.com>
2022-10-07 14:06:52 +0200

    Add script.sh

---
+++ script.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+if [ $# != 2 ]; then
+  echo "Usage: $0 <file containing paths to folders which will be backed up> <s3:// url to bucket [and folder in it] without ending />"  exit
+fi
+
+PATHS="$1"
+DIR="$(dirname $0)"
+BUCKET="$2"
+STAMP=$(date +%Y-%m-%d)
+
+CURR_DIR=$DIR/$STAMP
+
+mkdir $DIR/$STAMP
+
+cat $PATHS | while read line; do
+  SERVICE=$(basename $line)
+  echo "Creating zip for $SERVICE"
+  zip "$CURR_DIR/${SERVICE}_$STAMP.zip" -r $line > /dev/null
+done
+
+echo "Uploading to AWS"
+aws s3 cp $CURR_DIR $BUCKET/$STAMP/ --recursive --storage-class INTELLIGENT_TIERING
+rm -rf $CURR_DIR
